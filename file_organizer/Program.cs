a99855a25858215fba8dd28bf8ff1bf5b8adba7c
Richard <MrQuestionMark0_0@outlook.com>
2026-02-09 19:22:50 +0100

    created .NET CLI project

---
+++ file_organizer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace file_organiser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("loop gaat loop :)");
+            }
+        }
+    }
+}
