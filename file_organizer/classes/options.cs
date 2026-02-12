29fef9a995d31b2d7d84fb07c3b4ee1a7b6a21dc
Richard <MrQuestionMark0_0@outlook.com>
2026-02-12 13:27:37 +0100

    created the options class
    namespace: file_organiser
    
    methods:
    
    - organise()
    - clean()
    - move()

---
+++ file_organizer/classes/options.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace file_organizer
+{
+    public class options
+    {
+        private string path;
+
+        //public string organise()
+        //{
+            
+        //}
+
+        //public string clean(string path)
+        //{
+
+        //}
+
+        //public string move(string path)
+        //{
+
+        //}
+
+
+    }
+}
