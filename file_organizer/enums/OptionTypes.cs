618ac066033a43f45320dcb9c4a3b2051e05bf00
Richard <MrQuestionMark0_0@outlook.com>
2026-02-12 13:29:51 +0100

    added an enum (OptionTypes)
    With this enum i can easily check if options entered by the user are valid or not.

---
+++ file_organizer/enums/OptionTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace file_organizer
+{
+    public enum OptionTypes
+    {
+        CLEAN,
+        MOVE,
+        ORGANISE
+    }
+}
