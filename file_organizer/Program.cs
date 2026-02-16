df9f048f11770e8347e131e85798ebe2d1f634dd
Richard <MrQuestionMark0_0@outlook.com>
2026-02-16 20:23:35 +0100

    edited program.cs (main)
    The user can now input a path. If the path is incorrect the user gets asked to re-enter the path.
    
    After that the user can choose what they want to do with the path (list of options get printed by a loop through the enum list).
    
    The logic where the object is created and the option is executed is still in progress

--- file_organizer/Program.cs
+++ file_organizer/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using file_organiser;
 using file_organizer.data;
+using file_organizer;
 
 namespace main
 {
@@ -14,10 +15,43 @@ namespace main
         static void Main(string[] args)
         {
             Console.WriteLine(art.intro_text_banner());
-            //while (true)
-            //{
-            //    Console.WriteLine("loop gaat loop :)");
-            //}
+
+            Console.WriteLine();
+            Console.WriteLine("welcome to file organiser!");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a path to organise:");
+                var path = Console.ReadLine();
+
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine("nice");
+
+                    Console.WriteLine("options:");
+                    var OptionTypes_values = Enum.GetValues(typeof(OptionTypes));
+                    foreach (OptionTypes enum_option in OptionTypes_values)
+                    {
+                        Console.WriteLine(enum_option.ToString());
+                    }
+
+                    var chosen_option = Console.ReadLine();
+
+                    if (chosen_option != null)
+                    {
+                        //if (Enum.Parse<OptionTypes>(chosen_option) in OptionTypes_values) 
+                        //{
+                        //    program obj = new program();
+                        //    obj.process_option(path, chosen_option);
+                        //}
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("this path doesn't exist, please try again");
+                }
+            }
         }
     }
 }
