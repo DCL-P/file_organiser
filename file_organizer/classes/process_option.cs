02270c6003295f0c28766e07015169358b0dfc92
Richard <164242635+DCL-P@users.noreply.github.com>
2026-02-19 16:23:46 +0100

    edited program (renamed to process_option)
    made some progress on the process method. registered all the option types in vars in order to process every possible option.

---
+++ file_organizer/classes/process_option.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using file_organizer;
+using main;
+
+namespace file_organiser.program
+{
+    public class process_option
+    {
+        public string Path;
+        public OptionTypes Option;
+
+        public void constructor(string path, OptionTypes option)
+        {
+            Path = path;
+            Option = option;
+        }
+
+        public string process(string path, string option)
+        {
+            var CLEAN = Enum.Parse<OptionTypes>("CLEAN");
+            var MOVE = Enum.Parse<OptionTypes>("MOVE");
+            var ORGANISE = Enum.Parse<OptionTypes>("ORGANISE");
+
+            if (option is CLEAN)
+            {
+                return "bam";
+            }
+
+            return "the given option is not available. Please try again";
+        }
+    }
+}
